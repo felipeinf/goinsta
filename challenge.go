0fce4c6ab0738ce2b3138efb07264098aab2dea4
felipeinf <felipe.concha.a@mail.pucv.cl>
2020-11-07 22:04:24 -0300

    feat(challenge): set logged user in account

--- challenge.go
+++ challenge.go
@@ -132,6 +132,8 @@ func (challenge *Challenge) SendSecurityCode(code string) error {
 		if err == nil {
 			*challenge = *resp.Challenge
 			challenge.insta = insta
+			challenge.LoggedInUser.inst = insta
+			insta.Account = challenge.LoggedInUser
 		}
 	}
 	return err
